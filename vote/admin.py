ceb5b24168c158aef8997305a4d1b6c901a64073
Per <123196452+noxaur@users.noreply.github.com>
2026-08-25 15:19:18 +0200

    added 8 candidates, missing Benjamin

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ClassName, Candidate
+
+admin.site.register(ClassName)
+admin.site.register(Candidate)
--- vote/models.py
+++ vote/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class ClassName(models.Model):
     name = models.CharField(max_length=4)
 
@@ -9,14 +8,17 @@ class ClassName(models.Model):
 
 class Candidate(models.Model):
     forename = models.CharField(max_length=20)
-    surname = models.CharField(max_length=60, null=True)
-    date_of_birth = models.DateField(null=True)
+    surname = models.CharField(max_length=60)
+    date_of_birth = models.DateField(null=True, blank=True)
 
-    picture_happy = models.ImageField()
-    picture_happy = models.ImageField()
-    picture_sad = models.ImageField()
+    picture_happy = models.ImageField(null=True, blank=True)
+    picture_neutral = models.ImageField(null=True, blank=True)
+    picture_sad = models.ImageField(null=True, blank=True)
 
     class_name = models.ForeignKey(ClassName, on_delete=models.CASCADE) # TODO: change 
-    party_code = models.CharField(max_length=5, null=True)
-    party = models.CharField(max_length=40)
-    description = models.TextField(null=True)
+    party_code = models.CharField(max_length=5, null=True, blank=True)
+    party = models.CharField(max_length=40, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return " ".join([self.forename, self.surname, ":", self.party])
