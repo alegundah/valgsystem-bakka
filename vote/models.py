da4266fc2d27ef43729729d2326578708ce8ff18
Per <123196452+noxaur@users.noreply.github.com>
2026-08-25 14:37:30 +0200

    Made candidate and classname database entries.

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class ClassName(models.Model):
+    name = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.name
+
+class Candidate(models.Model):
+    forename = models.CharField(max_length=20)
+    surname = models.CharField(max_length=60, null=True)
+    date_of_birth = models.DateField(null=True)
+
+    picture_happy = models.ImageField()
+    picture_happy = models.ImageField()
+    picture_sad = models.ImageField()
+
+    class_name = models.ForeignKey(ClassName, on_delete=models.CASCADE) # TODO: change 
+    party_code = models.CharField(max_length=5, null=True)
+    party = models.CharField(max_length=40)
+    description = models.TextField(null=True)
