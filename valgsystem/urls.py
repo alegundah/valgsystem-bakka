6f59725d983369868ff75f26fe9bc5b619316e82
Per <123196452+noxaur@users.noreply.github.com>
2026-08-27 16:44:24 +0200

    update readme, + some

--- valgsystem/urls.py
+++ valgsystem/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("vote/", include("vote.urls")),
+    path("", include("vote.urls")),
     path('admin/', admin.site.urls),
 ]
