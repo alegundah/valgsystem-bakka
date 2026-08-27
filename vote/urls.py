26e0574239ca4f53ae00ef021d915167a50a1f6f
Zajvyj <dimazajvyj0@gmail.com>
2026-08-27 23:22:43 +0200

    Statistics

--- vote/urls.py
+++ vote/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 urlpatterns = [
     path("", views.VoteView.as_view(), name="vote"),
-    path("login/", views.LoginForm.as_view(), name="login"),
+    path("login/", views.LoginView.as_view(), name="login"),
     path("takk/", views.ThanksView.as_view(), name="thanks"),
     path("brukere/", views.UserView.as_view(), name="vote_users"),
+    path("statistikk/", views.StatsView.as_view(), name="stats"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 ]
